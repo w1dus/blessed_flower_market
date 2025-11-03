bf343bb2e1ffed2b7e4488932f4f43e825020a81
Kim ji yeon <67830760+w1dus@users.noreply.github.com>
2025-11-03 17:50:52 +0900

    퍼블리싱 시작

--- script/script.js
+++ script/script.js
@@ -1,7 +1,36 @@
 
 
 document.addEventListener("DOMContentLoaded", function(e){
-
+  pc_menu_slide();
+  mobile_menu_toggle();
 })
 
+const mobile_menu_toggle = () => {
+  $('.mobile-menu-div .item').click(function(e){
+    if($(this).hasClass('sub')){
+      e.preventDefault();
+      $('.mobile-menu-div .item.sub').not(this).removeClass('show').siblings('.sub-menu-list').slideUp();
+      $(this).toggleClass("show");
+      $(this).siblings('.sub-menu-list').slideToggle();
+    }
+  });
+  //메뉴 오픈
+  $('header .menu-btn').click(function(){
+    $('.mobile-menu-div').addClass('show')
+  })
+  //메뉴 닫기
+  $('.mobile-menu-div .close-btn').click(function(){
+    $('.mobile-menu-div').removeClass('show')
+  })
+}
 
+const pc_menu_slide = () => {
+  $('header .menu-div .menu-list').mouseenter(function(){
+      $('.pc-slide-menu').slideDown(200);
+      $('.pc-menu-close').addClass('show');
+  })
+  $('.pc-menu-close').mouseenter(function(){
+    $('.pc-slide-menu').slideUp();
+    $('.pc-menu-close').removeClass('show');
+  })
+}
